07e05f1b4c192873c438557d85bffb2f465bbe23
Raphaël <raphael.nl.roulin@gmail.com>
2024-05-06 15:35:01 +0200

    commit

--- TP4/ex2.py
+++ TP4/ex2.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import csv
 import os
 import pickle
+import control
 
 class Etudiant :
     @classmethod
@@ -109,12 +110,11 @@ class Etudiant :
 
 class Groupe :
     @classmethod
-    def charger_csv(cls, file) :
+    def charger(cls, file) :
         if not os.path.isfile(file) :
             raise ValueError("Impossible de trouver le fichier.")
         
         new_gr = Groupe(file)
-        
         with open(file, "r", newline="") as f:
             reader = csv.DictReader(f)
             for row in reader :
@@ -122,21 +122,6 @@ class Groupe :
 
         return new_gr
     
-    @classmethod
-    def loadFromBin(cls, file) :
-        if not os.path.isfile(file) :
-            raise ValueError("Impossible de trouver le fichier.")
-        
-        new_gr = Groupe(file)
-        with open(file, "rb") as f:
-            list_et = pickle.load(f)
-
-        for etu in list_et :
-            new_gr.addEtu(etu)
-
-        return new_gr
-        
-
 
     def __init__(self, file = "Etu") :
         self._etu = []
@@ -157,15 +142,11 @@ class Groupe :
     def file(self) :
         return self._file + (".csv" if (self._file[-4:] != ".csv") else "")
     
-    @property
-    def file_bin(self) :
-        return self._file + (".bin" if (self._file[-4:] != ".bin") else "")
-    
     @file.setter
     def file(self, val) :
         self._file = str(val)
 
-    def sauvegarder_csv(self) :
+    def sauvegarder(self) :
         with open(self.file, "w", newline = "") as f :
             header = ["Nom", "Date_de_naissance", "Gpa", "Connais_python"]
             writer = csv.DictWriter(f, fieldnames=header)
@@ -173,47 +154,38 @@ class Groupe :
             writer.writeheader()
             for student in self.etu :
                 writer.writerow(student.to_dict())
-
-    def sauvegarderBin(self) :
-        with open(self.file_bin, "wb") as f:
-            pickle.dump(self.etu, f)
-
             
 
 def main() :
-    etu1 = Etudiant("RR", 2005, 5, True)
-    etu2 = Etudiant("AP", 2005, 5, True)
 
     list_et = Groupe()
-    list_et.addEtu(etu1)
-    list_et.addEtu(etu2)
+    
+    while control.secureAskType(int, "Continuer ? : (0/1) : ", lambda x : (x == 0 or x == 1)) :
+        creerEtu(list_et)
+
 
     print(list_et.file)
-    list_et.sauvegarder_csv()
-    list_et.sauvegarderBin()
+    list_et.sauvegarder()
     
     print("L1")
     for etu in list_et.etu :
         print(etu)
 
-    list_etu_2 = Groupe.charger_csv("Etu.csv")
-    list_etu_3 = Groupe.loadFromBin("Etu.bin")
+    list_etu_2 = Groupe.charger("Etu.csv")
 
     print("L2: ")
     for etu in list_etu_2.etu :
         print(etu)
 
-    print("L3: ")
-    for etu in list_etu_3.etu :
-        print(etu)
+def creerEtu(group : Groupe) :
+    nom = control.secureAskType(str, "Entrez le nom de l'étudiant : ")
+    naiss = control.secureAskType(int, "Entrez l'année de naissance de l'étudiant : ", lambda x : 1900 < x < 2025, "Vous n'avez pas entré un entier", "L'année n'est pas valide. Réessayez : ")
+    gpa = control.secureAskType(float, "Entrez la gpa de l'étudiant : ", lambda x : 0 <= x <= 5,  "Vous n'avez pas entré un flottant", "La gpa n'a pas une valeur valide. Réessayez : ")
+    connais_python = control.secureAskType(int, "L'étudiant connait-il python ? (0/1) : ", lambda x : (x == 0 or x == 1))
+    group.addEtu(Etudiant(nom, naiss, gpa, bool(connais_python)))
 
 if __name__ == "__main__" :
+
     main()
 
 
-"""Ici, le mode texte nous permet de pouvoir visualiser les données sans utiliser directement le programme.
-Le mode binaire permet, au contraire, de passer obligatoirement le programme. Cela peut être utile lorsque l'on veut
-obfusquer l'accès aux données.
-
-Le mode binaire permet plus facilement de stocker des données complexes, comme par exemple un arbre binaire de recherche
-qu'il est plus dur, de part leur nature, d'enregistrer."""
---
+++ TP4/ex3.py
@@ -0,0 +1,71 @@
+import ex2
+import os
+import pickle
+import control
+
+class GroupeBin(ex2.Groupe) :
+    @classmethod
+    def charger(cls, file) :
+        if not os.path.isfile(file) :
+            raise ValueError("Impossible de trouver le fichier.")
+        
+        new_gr = ex2.Groupe(file)
+        with open(file, "rb") as f:
+            list_et = pickle.load(f)
+
+        for etu in list_et :
+            new_gr.addEtu(etu)
+
+        return new_gr
+    
+    @property
+    def file(self) :
+        return self._file + (".bin" if (self._file[-4:] != ".bin") else "")
+    
+    @file.setter
+    def file(self, val) :
+        self._file = str(val)
+
+    def sauvegarder(self) :
+        with open(self.file, "wb") as f:
+            pickle.dump(self.etu, f)
+
+
+def main() :
+
+    list_et = GroupeBin()
+    
+    while control.secureAskType(int, "Continuer ? : (0/1) : ", lambda x : (x == 0 or x == 1)) :
+        creerEtu(list_et)
+
+
+    print(list_et.file)
+    list_et.sauvegarder()
+    
+    print("L1")
+    for etu in list_et.etu :
+        print(etu)
+
+    list_etu_2 = GroupeBin.charger("Etu.bin")
+
+    print("L2: ")
+    for etu in list_etu_2.etu :
+        print(etu)
+
+def creerEtu(group : GroupeBin) :
+    nom = control.secureAskType(str, "Entrez le nom de l'étudiant : ")
+    naiss = control.secureAskType(int, "Entrez l'année de naissance de l'étudiant : ", lambda x : 1900 < x < 2025, "Vous n'avez pas entré un entier", "L'année n'est pas valide. Réessayez : ")
+    gpa = control.secureAskType(float, "Entrez la gpa de l'étudiant : ", lambda x : 0 <= x <= 5,  "Vous n'avez pas entré un flottant", "La gpa n'a pas une valeur valide. Réessayez : ")
+    connais_python = control.secureAskType(int, "L'étudiant connait-il python ? (0/1) : ", lambda x : (x == 0 or x == 1))
+    group.addEtu(ex2.Etudiant(nom, naiss, gpa, bool(connais_python)))
+
+if __name__ == "__main__" :
+    main()
+
+
+"""Ici, le mode texte nous permet de pouvoir visualiser les données sans utiliser directement le programme.
+Le mode binaire permet, au contraire, de passer obligatoirement le programme. Cela peut être utile lorsque l'on veut
+obfusquer l'accès aux données.
+
+Le mode binaire permet plus facilement de stocker des données complexes, comme par exemple un arbre binaire de recherche
+qu'il est plus dur, de part leur nature, d'enregistrer."""
