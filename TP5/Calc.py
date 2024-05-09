a6e449bb41fc6a969f0ad4e1963154870c28a336
Raphaël <raphael.nl.roulin@gmail.com>
2024-05-09 15:25:01 +0200

    Calc works

--- TP5/Calc.py
+++ TP5/Calc.py
@@ -1,4 +1,111 @@
 import tkinter as tk
+import math
+
+
+class Calculation :
+    def __init__(self, text : str, x_val : float | None = None) :
+        text = text.replace("sin", "s").replace("cos", "c").replace("tan", "t").replace("sqrt", "r").replace("²", "^2").replace("pi", f"{math.pi}")
+        if x_val != None :
+            text = text.replace("x", x_val)
+
+        self.text_left_member = ""
+        self.text_right_member = ""
+        self.operation = ""
+
+        parenthesis_level = 0
+        parenthesis_level_op = None
+        is_op_passed = False
+
+        for char in text :
+            if char == "(" :
+                parenthesis_level += 1
+            elif char == ")" :
+                parenthesis_level -= 1
+
+            if char in "cstr+-/*^" :
+                if parenthesis_level_op == None or parenthesis_level_op > parenthesis_level :
+                    is_op_passed = True
+                    self.text_left_member += self.text_right_member
+                    self.operation = char
+                    parenthesis_level_op = parenthesis_level
+
+                elif parenthesis_level_op == parenthesis_level :
+                    if self.operation in "cstr/*^" and char in "+-" :
+                        self.text_left_member += self.text_right_member
+                        self.operation = char
+
+                    elif self.operation in "cstr^" and char in "*/" :
+                        self.text_left_member += self.text_right_member
+                        self.operation = char
+                
+            if is_op_passed :
+                self.text_right_member += char
+            else :
+                self.text_left_member += char
+
+        self.text_right_member = self.text_right_member.lstrip(self.operation)
+
+        if (parenthesis_level_op != None) :
+            for i in range(parenthesis_level_op) :
+                print("Here")
+                self.text_left_member = self.text_left_member.lstrip('(')
+                self.text_right_member = self.text_right_member.rstrip(')')
+
+        print(f"Output : op : {self.operation}, left member : {self.text_left_member}, right member : {self.text_right_member}")
+        if self.operation != "" :
+            if self.text_right_member != "" :
+                self.right_member = Calculation(self.text_right_member)
+            if self.text_left_member != "" :
+                self.left_member = Calculation(self.text_left_member)
+
+    def Calculate(self) -> float:
+        match self.operation :
+            case "" :
+                if self.text_left_member != "" :
+                    return float(self.text_left_member)
+                else :
+                    return float(self.text_left_member)
+                
+            case "+" :
+                return self.right_member.Calculate() + self.left_member.Calculate()
+            
+            case "-" :
+                if self.text_left_member != "" :
+                    return self.left_member.Calculate() - self.right_member.Calculate()
+                return -self.right_member.Calculate()
+                
+            case "*" :
+                if self.text_left_member == "" :
+                    return self.right_member.Calculate()
+                return self.left_member.Calculate() * self.right_member.Calculate()
+            
+            case "/" :
+                return self.left_member.Calculate() / self.right_member.Calculate() 
+
+            case "c" :
+                if self.text_left_member == "" :
+                    return math.cos(self.right_member.Calculate())
+                return  self.left_member.Calculate()*math.cos(self.right_member.Calculate())
+            
+            case "s" :
+                if self.text_left_member == "" :
+                    return math.sin(self.right_member.Calculate())
+                return  self.left_member.Calculate()*math.sin(self.right_member.Calculate())
+            
+            case "t" :
+                if self.text_left_member == "" :
+                    return math.tan(self.right_member.Calculate())
+                return  self.left_member.Calculate()*math.tan(self.right_member.Calculate())
+            
+            case "r" :
+                if self.text_left_member == "" :
+                    return math.sqrt(self.right_member.Calculate())
+                return self.left_member.Calculate()*math.sqrt(self.right_member.Calculate())
+            
+            case "^" :
+                return self.left_member.Calculate() ** self.left_member.Calculate()
+                
+        
 
 class CalculatriceButtonChar(tk.Button) :
     def __init__(self, parent, char, calc_host) :
@@ -11,10 +118,10 @@ class CalculatriceButtonChar(tk.Button) :
         self.calc_host.addChar(str(self.char))
 
 
-
 class Calculatrice(tk.Tk) :
     def __init__(self, size = (400, 500)) :
         tk.Tk.__init__(self)
+        self.trigger_reset = False
         self._txt = tk.StringVar()
         self.config()
         self.geometry(f"{size[0]}x{size[1]}+100+100")
@@ -46,12 +153,41 @@ class Calculatrice(tk.Tk) :
         self.do_opeation_button = tk.Button(self.frame, command=self.doCalculation, text="=", background="#5585f0", relief="flat")
         self.do_opeation_button.grid(row = 6, column = 0, padx=5, pady=5, sticky="nsew")
 
+        btm_color = "#4065d0"
+
+        self.graphic_mode_button = tk.Button(self.frame, command=self.goGraphicMode, text="Mode graphique", background=btm_color, relief="flat")
+        self.graphic_mode_button.grid(row = 7, column = 1, padx=5, pady=5, sticky="nsew", columnspan=2)
+
+        self.historique_mode_button = tk.Button(self.frame, command=self.goHistoricMode, text="Historique", background=btm_color, relief="flat")
+        self.historique_mode_button.grid(row = 7, column = 3, padx=5, pady=5, sticky="nsew", columnspan=2) 
+
+        self.clear_button = tk.Button(self.frame, command=self.clearArea, text="cls", background=btm_color, relief="flat")
+        self.clear_button.grid(row = 7, column = 0, padx=5, pady=5, sticky="nsew", columnspan=1)       
+
+
     def addChar(self, char) :
-        self._txt.set(self._txt.get() + char)
+        if not self.trigger_reset :
+            self._txt.set(self._txt.get() + char)
+        else :
+            self._txt.set(char)
+            self.trigger_reset = False
+
+    def clearArea(self) :
+        self._txt.set("")
 
     def doCalculation(self) :
-        pass
+        try :
+            self._txt.set(self._txt.get() + f" = {Calculation(self._txt.get()).Calculate()}")
+        except RecursionError:
+            self._txt.set("Opération saisie non valide")
+        self.trigger_reset = True
         
+    def goGraphicMode(self) :
+        pass
+
+    def goHistoricMode(self) :
+        pass
+
 
 
 def main() :
