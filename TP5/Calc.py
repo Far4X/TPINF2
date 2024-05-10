a2d26e58f00c850329f19ed4be6aeafe5e5d75b1
Raphaël <raphael.nl.roulin@gmail.com>
2024-05-10 18:15:39 +0200

    Fix

--- TP5/Calc.py
+++ TP5/Calc.py
@@ -28,20 +28,24 @@ class Calculation :
                     self.text_left_member += self.text_right_member
                     self.operation = char
                     parenthesis_level_op = parenthesis_level
+                    self.text_right_member = ""
 
                 elif parenthesis_level_op == parenthesis_level :
                     if self.operation in "cstr/*^" and char in "+-" :
                         self.text_left_member += self.text_right_member
+                        self.text_right_member = ""
                         self.operation = char
 
                     elif self.operation in "cstr^" and char in "*/" :
                         self.text_left_member += self.text_right_member
                         self.operation = char
+                        self.text_right_member = ""
                 
             if is_op_passed :
                 self.text_right_member += char
             else :
                 self.text_left_member += char
+
 
         self.text_right_member = self.text_right_member.lstrip(self.operation)
 
@@ -107,6 +111,18 @@ class Calculation :
                 
         
 
+class Historal :
+    def __init__(self) :
+        self._list_op = []
+
+    def opEffectued(self, op : str) :
+        self._list_op.append(op)
+
+    def getOps(self, page, lenght) :
+        if lenght > len(self._list_op) :
+            return (self._list_op[-1-lenght*(page+1):-1-lenght*page])
+        return (self._list_op)
+
 class CalculatriceButtonChar(tk.Button) :
     def __init__(self, parent, char, calc_host) :
         tk.Button.__init__(self, parent, command=self.clickedOn, text=char, background = ("#909090" if str(char) not in "0123456789" else "#aaa"), relief="flat")
@@ -117,13 +133,19 @@ class CalculatriceButtonChar(tk.Button) :
     def clickedOn(self) :
         self.calc_host.addChar(str(self.char))
 
+    def setChar(self, char) :
+        self.config(text = char)
+        self.char = char
+
 
 class Calculatrice(tk.Tk) :
     def __init__(self, size = (400, 500)) :
         tk.Tk.__init__(self)
+        self.graph_mode = False
         self.trigger_reset = False
         self._txt = tk.StringVar()
-        self.config()
+        self.historal = Historal()
+        #self.config()
         self.geometry(f"{size[0]}x{size[1]}+100+100")
         self.frame = tk.Frame(self, padx= 20, pady=20, bg = "#202030")
         self.separator = tk.Canvas(self.frame, height=20, bg = "#202030", relief="flat", highlightthickness=0)
@@ -137,11 +159,11 @@ class Calculatrice(tk.Tk) :
         self.separator.grid(row = 1, column = 0, columnspan= 5, sticky="ew")
         self.separator.columnconfigure(0, weight=1)
 
-        list_btns_char =   [("cos", 2, 0), ("sin", 2, 1), ("tan", 2, 2), ('pi', 2, 3), ('²', 2, 4),
-                            (1, 3, 0),     (2, 3, 1),     (3, 3, 2),     ('+', 3, 3),  ('sqrt', 3, 4),
-                            (4, 4, 0),     (5, 4, 1),     (6, 4, 2),     ('-', 4, 3),  ('^', 4, 4),
-                            (7, 5, 0),     (8, 5, 1),     (9, 5, 2),     ('*', 5, 3),  ('(', 5, 4),
-                                           (0, 6, 1),     (".", 6, 2),   ('/', 6, 3),  (')', 6, 4),
+        list_btns_char =   [("cos", 2, 0), ("sin", 2, 1), ("tan", 2, 2), ('pi', 2, 3), ('sqrt', 2, 4),
+                            (1, 3, 0),     (2, 3, 1),     (3, 3, 2),     ('+', 3, 3),  ('^', 3, 4),
+                            (4, 4, 0),     (5, 4, 1),     (6, 4, 2),     ('-', 4, 3),  ('(', 4, 4),
+                            (7, 5, 0),     (8, 5, 1),     (9, 5, 2),     ('*', 5, 3),  (')', 5, 4),
+                                           (0, 6, 1),     (".", 6, 2),   ('/', 6, 3),
                         ]
         
         for btn_data in list_btns_char :
@@ -149,6 +171,9 @@ class Calculatrice(tk.Tk) :
             btn.grid(row = btn_data[1], column=btn_data[2], sticky="ewns", padx=5, pady = 5)
             self.frame.columnconfigure(btn_data[2], weight=1)
             self.frame.rowconfigure(btn_data[1], weight=1)
+
+        self.button_multifunc = CalculatriceButtonChar(self.frame, "²", self)
+        self.button_multifunc.grid(row = 6, column=4, sticky="ewns", padx=5, pady = 5)
 
         self.do_opeation_button = tk.Button(self.frame, command=self.doCalculation, text="=", background="#5585f0", relief="flat")
         self.do_opeation_button.grid(row = 6, column = 0, padx=5, pady=5, sticky="nsew")
@@ -176,14 +201,24 @@ class Calculatrice(tk.Tk) :
         self._txt.set("")
 
     def doCalculation(self) :
-        try :
-            self._txt.set(self._txt.get() + f" = {Calculation(self._txt.get()).Calculate()}")
-        except RecursionError:
-            self._txt.set("Opération saisie non valide")
-        self.trigger_reset = True
+        self.historal.opEffectued(self._txt.get())
+        if not self.graph_mode :
+            try :
+                self._txt.set(self._txt.get() + f" = {Calculation(self._txt.get()).Calculate()}")
+            except RecursionError:
+                self._txt.set("Opération saisie non valide")
+            self.trigger_reset = True
         
     def goGraphicMode(self) :
-        pass
+        if not self.graph_mode :
+            self.graph_mode = True
+            self.graphic_mode_button.configure(bg="#209010")
+            self.button_multifunc.setChar("x")
+        else :
+            self.graph_mode = False
+            self.graphic_mode_button.configure(bg="#4065d0")
+            self.button_multifunc.setChar("²")
+
 
     def goHistoricMode(self) :
         pass
